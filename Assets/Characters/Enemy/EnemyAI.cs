b493783ab04cfa85f3e0653b89b3202d6e1d3591
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-03-01 05:24:19 +0530

    Created enemy and added nav mesh component to enemy

---
+++ Assets/Characters/Enemy/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent navMeshAgent;
+    [SerializeField] GameObject target; 
+    void Start()
+    {
+        navMeshAgent=GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+    }
+}
