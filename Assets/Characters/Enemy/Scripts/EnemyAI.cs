e02efca1db87d6d683103ff8ff030717eab83356
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-03-08 22:16:54 +0530

    now enemy chase the player if player in chase range of the enemy

---
+++ Assets/Characters/Enemy/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange=5f;
+    float distanceToTarget = Mathf.Infinity;
+    NavMeshAgent navMeshAgent; 
+    void Start()
+    {
+        navMeshAgent=GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position,transform.position);
+        if (distanceToTarget<=chaseRange) { navMeshAgent.SetDestination(target.position); }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawWireSphere(transform.position,chaseRange);
+    }
+}
