94b00ccc8abd28d691ebe16ac0d621ca686de50f
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-03-09 20:49:00 +0530

    added PlayerHealth.cs

---
+++ Assets/Characters/FirstPersonCharacter/Scripts/PlayerHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    void TakeDamage(float damage) {
+        health -= damage;
+        if (health<=0) {
+            print("you are dead!!");
+        }
+    }
+}
