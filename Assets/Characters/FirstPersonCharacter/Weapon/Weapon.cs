78a2c4f724ad71b17183ad98f38fe5a48a1fc4fb
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-03-09 18:16:44 +0530

    Added Weapon.cs to manage weapon of player and added shoot method to Weapon.cs

---
+++ Assets/Characters/FirstPersonCharacter/Weapon/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 10f;
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    private void Shoot()
+    {
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        print(hit.transform.name);
+    }
+}
