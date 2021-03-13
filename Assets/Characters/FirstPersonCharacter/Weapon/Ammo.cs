0ff9a3210bc0269e7fd57f5e6f4034515c4f8be4
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-03-13 18:43:24 +0530

    Implemented Ammo feature to Game

---
+++ Assets/Characters/FirstPersonCharacter/Weapon/Ammo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] int ammoCount=10;
+
+    public void ReduceAmmoCount() {
+        ammoCount -= 1;
+    }
+
+    public int GetCurrentAmmoCount() {
+        return ammoCount;
+    }
+}
