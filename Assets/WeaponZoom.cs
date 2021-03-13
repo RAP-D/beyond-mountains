73ddad87452fbe8ff37934945865c7b20b40e5fe
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-03-13 17:28:55 +0530

    Implemented WeaponZoom class to handle weapon Zoom Feature

---
+++ Assets/WeaponZoom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    Camera camera;
+    [SerializeField] float ZoomInFOV = 30f;
+    [SerializeField] float ZoomOutFOV = 60f;
+    bool isZoomToggle=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1)){
+            isZoomToggle = !isZoomToggle;
+        }
+        if (isZoomToggle) { ZoomIn(); }
+        else { ZoomOut(); }
+    }
+
+    public void ZoomIn()
+    {
+        camera.fieldOfView = ZoomInFOV;
+    }
+
+    public void ZoomOut() {
+        camera.fieldOfView = ZoomOutFOV;
+    }
+}
